5d08d20656584915b43eb10773911da69c09969a
xiangbc <XiangBC123456>
2016-08-01 16:43:58 +0800

    提交Dialog

--- app/src/main/java/www/lvchehui/com/carteam/view/dlg/CWayDlg.java
+++ app/src/main/java/www/lvchehui/com/carteam/view/dlg/CWayDlg.java
@@ -1,17 +1,14 @@
 package www.lvchehui.com.carteam.view.dlg;
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
-
 import www.lvchehui.com.carteam.R;
-
 /**
  * 作者：V先生 on 2016/8/1 15:36
- * 作用：
+ * 作用：设置默认的弹框
  */
 public class CWayDlg extends Dialog implements View.OnClickListener {
     public CWayDlg(Context context) {
