9911b2e1c27876b6dc319ee06cd788571d54370c
xiangbc <XiangBC123456>
2016-08-01 13:59:33 +0800

    提交App

--- app/src/main/java/www/lvchehui/com/carteam/app/App.java
+++ app/src/main/java/www/lvchehui/com/carteam/app/App.java
@@ -1,9 +1,19 @@
 package www.lvchehui.com.carteam.app;
 
+import android.app.Activity;
 import android.app.Application;
+
+import com.google.gson.Gson;
 
 import org.xutils.BuildConfig;
 import org.xutils.x;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+import www.lvchehui.com.carteam.tools.XgoLog;
+import www.lvchehui.com.carteam.view.toast.ToastManager;
 
 /**
  * 作者：V先生 on 2016/7/29 20:06
@@ -11,10 +21,11 @@ import org.xutils.x;
  */
 public class App extends Application {
     private static App mApp;
+    public List<WeakReference<Activity>> aliveActivitys = new ArrayList<>();
     @Override
     public void onCreate() {
         super.onCreate();
-
+        mApp = this;
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG);//是否输出debug日记，开启会影响性能
     }
@@ -25,5 +36,31 @@ public class App extends Application {
             mApp = new App();
         }
         return mApp;
+    }
+
+    public <T>T getBeanFromJson(String ret,Class<T> c){
+        T bean = null;
+        try {
+            bean = new Gson().fromJson(ret, c);
+        }catch(Exception e)
+        {
+            ToastManager.getManager().show("Gson解析错误:" + e.toString());
+            XgoLog.e("Gson解析错误");
+        }
+        return bean;
+    }
+
+    public void finishAllActivity(){
+        for (int i = 0;i < aliveActivitys.size();i++)
+        {
+            if (aliveActivitys.get(i) != null)
+            {
+                aliveActivitys.get(i).get().finish();
+            }
+        }
+    }
+
+    public Activity getTopActivity(){
+        return aliveActivitys.get(aliveActivitys.size() - 1).get();
     }
 }
