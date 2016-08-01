ea5713ac52b8a142738a190279915615f890d3ae
xiangbc <XiangBC123456>
2016-08-01 16:35:34 +0800

    提交接口的设置

--- app/src/main/java/www/lvchehui/com/carteam/http/ComCb.java
+++ app/src/main/java/www/lvchehui/com/carteam/http/ComCb.java
@@ -1,20 +1,14 @@
 package www.lvchehui.com.carteam.http;
 
-import android.widget.Toast;
-
-import org.xutils.common.Callback;
+import org.xutils.common.Callback.CommonCallback;
 import org.xutils.ex.HttpException;
-import org.xutils.x;
-
-import www.lvchehui.com.carteam.app.App;
 import www.lvchehui.com.carteam.tools.XgoLog;
-import www.lvchehui.com.carteam.view.toast.ToastManager;
 
 /**
  * 作者：V先生 on 2016/7/30 16:40
- * 作用：网络请求返回
+ * 作用：网络请求返回结果
  */
-public abstract class ComCb implements Callback.CommonCallback<String> {
+public  abstract class ComCb<ResultType> implements CommonCallback<ResultType> {
     String errStr = "";
     @Override
     public void onError(Throwable ex, boolean isOnCallback) {
@@ -31,18 +25,12 @@ public abstract class ComCb implements Callback.CommonCallback<String> {
             errStr = "网络错误";
         }
         XgoLog.e("errStr:" + errStr);
-        ToastManager.getManager().show("网络错误");
     }
-
     @Override
     public void onCancelled(CancelledException cex) {
         XgoLog.e("ex_onError:" + cex.toString());
-        ToastManager.getManager().show("ex_onError:" + cex.toString());
     }
-
     @Override
     public void onFinished() {
     }
-
-
 }
