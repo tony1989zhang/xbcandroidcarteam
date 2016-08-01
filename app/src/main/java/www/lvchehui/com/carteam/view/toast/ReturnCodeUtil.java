e53b0977181c4558c4e1a323566be3080ed349ac
xiangbc <XiangBC123456>
2016-08-01 14:36:46 +0800

    提交返回的错误码

---
+++ app/src/main/java/www/lvchehui/com/carteam/view/toast/ReturnCodeUtil.java
@@ -0,0 +1,39 @@
+package www.lvchehui.com.carteam.view.toast;
+
+import android.content.Context;
+
+import www.lvchehui.com.carteam.app.App;
+
+
+/**
+ * @author 张灿能
+ * 返回状态码
+ */
+public class ReturnCodeUtil {
+
+	private static void showNoticeToast(int stringResId) {
+		if (stringResId == 0) {
+			ToastManager.getManager().show("操作失败");
+		} else {
+			ToastManager.getManager().show(stringResId);
+		}
+
+	}
+
+	public static void showToast(int code) {
+		Context context = App.getInstance();
+		int stringResId = context.getResources().getIdentifier("return_code_" + code,
+				"string", "com.wbaiju.ichat");
+		showNoticeToast(stringResId);
+	}
+
+	public static void showToast(String code) {
+		Context context = App.getInstance();
+		int stringResId = context.getResources().getIdentifier("return_code_" + code,
+				"string", "com.lvchehui.www");
+		showNoticeToast(stringResId);
+	}
+
+	
+
+}
