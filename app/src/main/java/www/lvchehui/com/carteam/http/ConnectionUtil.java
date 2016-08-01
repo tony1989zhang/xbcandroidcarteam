cae7dafba0dad42d0ae1866014421161c5f25c89
xiangbc <XiangBC123456>
2016-08-01 16:19:50 +0800

    提交网络请求的方法Post Get 以及上传下载。

--- app/src/main/java/www/lvchehui/com/carteam/http/ConnectionUtil.java
+++ app/src/main/java/www/lvchehui/com/carteam/http/ConnectionUtil.java
@@ -1,70 +1,60 @@
 package www.lvchehui.com.carteam.http;
 
-import android.graphics.drawable.Drawable;
-import android.widget.ImageView;
-
 import org.xutils.common.Callback;
-import org.xutils.common.util.DensityUtil;
-import org.xutils.image.ImageOptions;
+import org.xutils.http.RequestParams;
 import org.xutils.x;
 
-import www.lvchehui.com.carteam.R;
-
-import static www.lvchehui.com.carteam.R.*;
+import java.util.Map;
+import java.util.Objects;
 
 /**
- * 作者：V先生 on 2016/8/1 14:40
+ * 作者：V先生 on 2016/8/1 16:05
  * 作用：
  */
 public class ConnectionUtil {
-    private Drawable drawableResult;
-    private ImageOptions imageOptions;
-    private ConnectionUtil(){
+    private static final String SERVER_URL = "192.168.1.66/api.php/main/";
 
-    }
-
-    private static ConnectionUtil connectionUtil;
-
-    public static final ConnectionUtil getInstance() {
-        if (connectionUtil == null)
+    //get方法
+    public static<T>Callback.Cancelable Get(String path, Map<String,String> map, Callback.CommonCallback<T> callback){
+        RequestParams params = new RequestParams(getUrl(path));
+        if (null != map)
         {
-            connectionUtil = new ConnectionUtil();
+            for (Map.Entry<String,String> entry:map.entrySet()){
+                params.addQueryStringParameter(entry.getKey(),entry.getValue());
+            }
         }
-        return  connectionUtil;
+        Callback.Cancelable cancelable = x.http().get(params, callback);
+        return cancelable;
     }
-    private void initImageOptions(){
-        if (null == imageOptions)
+
+    public static <T>Callback.Cancelable Post(String path, Map<String,Object> map, Callback.CommonCallback<T> callback){
+        RequestParams params = new RequestParams(getUrl(path));
+        if (null != map)
         {
-            imageOptions = new ImageOptions.Builder().setSize(DensityUtil.dip2px(120),DensityUtil.dip2px(120))
-                    .setRadius(DensityUtil.dip2px(5)).setCrop(true)
-                    .setImageScaleType(ImageView.ScaleType.CENTER_CROP).setLoadingDrawableId(mipmap.ic_launcher)
-                    .setFailureDrawableId(mipmap.ic_launcher).build();
+            for (Map.Entry<String,Object> entry:map.entrySet())
+            {
+               params.addParameter(entry.getKey(),entry.getValue());
+            }
         }
+        Callback.Cancelable cancelable = x.http().post(params, callback);
+        return cancelable;
     }
 
-    public Drawable loadImage(ImageView iv, String picUrl){
-        initImageOptions();
-        x.image().bind(iv,picUrl,imageOptions,new Callback.CommonCallback<Drawable>(){
-            @Override
-            public void onSuccess(Drawable result) {
-                drawableResult = result;
+    public static <T>Callback.Cancelable UpLoadFile(String path,Map<String,Object> map,Callback.CommonCallback<T> callback){
+        RequestParams params = new RequestParams(getUrl(path));
+        if (null != map)
+        {
+            for (Map.Entry<String,Object> entry:map.entrySet())
+            {
+                params.addParameter(entry.getKey(),entry.getValue());
             }
+        }
+        params.setMultipart(true);
+        Callback.Cancelable cancelable = x.http().post(params, callback);
+        return cancelable;
+    }
 
-            @Override
-            public void onError(Throwable ex, boolean isOnCallback) {
-                drawableResult = x.app().getResources().getDrawable(R.mipmap.ic_launcher);
-            }
-
-            @Override
-            public void onCancelled(CancelledException cex) {
-                drawableResult = x.app().getResources().getDrawable(R.mipmap.ic_launcher);
-            }
-
-            @Override
-            public void onFinished() {
-                drawableResult = x.app().getResources().getDrawable(R.mipmap.ic_launcher);
-            }
-        });
-        return  drawableResult;
+    private static String getUrl(String path) {
+        return "http://" + SERVER_URL + path;
     }
 }
