eee895dee094b7aa3f7cbcf545c2da8541a47afe
xiangbc <XiangBC123456>
2016-08-01 14:36:11 +0800

    在Application里初始化数据库。

--- app/src/main/java/www/lvchehui/com/carteam/app/App.java
+++ app/src/main/java/www/lvchehui/com/carteam/app/App.java
@@ -6,6 +6,7 @@ import android.app.Application;
 import com.google.gson.Gson;
 
 import org.xutils.BuildConfig;
+import org.xutils.DbManager;
 import org.xutils.x;
 
 import java.lang.ref.WeakReference;
@@ -22,12 +23,20 @@ import www.lvchehui.com.carteam.view.toast.ToastManager;
 public class App extends Application {
     private static App mApp;
     public List<WeakReference<Activity>> aliveActivitys = new ArrayList<>();
+    private  DbManager.DaoConfig daoConfig;
+    public DbManager.DaoConfig getDaoConfig(){
+        if (null == daoConfig){
+            initDb();
+        }
+        return  daoConfig;
+    }
     @Override
     public void onCreate() {
         super.onCreate();
         mApp = this;
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG);//是否输出debug日记，开启会影响性能
+        initDb();
     }
 
     public static App getInstance(){
@@ -38,6 +47,14 @@ public class App extends Application {
         return mApp;
     }
 
+    public void initDb(){
+        new DbManager.DaoConfig().setDbName("xbc_cart_team_db").setDbVersion(1).setDbUpgradeListener(new DbManager.DbUpgradeListener() {
+            @Override
+            public void onUpgrade(DbManager db, int oldVersion, int newVersion) {
+                    XgoLog.e("数据库更新" + newVersion);
+            }
+        });//数据库更新操作
+    }
     public <T>T getBeanFromJson(String ret,Class<T> c){
         T bean = null;
         try {
