aa1b018b9a99bf7da1e6ae4b982d60044b60fdb5
xiangbc <XiangBC123456>
2016-08-02 17:57:20 +0800

    提交home

--- app/src/main/java/www/lvchehui/com/carteam/activities/HomeAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/activities/HomeAct.java
@@ -2,6 +2,7 @@ package www.lvchehui.com.carteam.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.xutils.view.annotation.ContentView;
@@ -10,31 +11,33 @@ import org.xutils.view.annotation.ViewInject;
 
 import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.base.BaseAct;
-import www.lvchehui.com.carteam.bean.LoginBean;
-import www.lvchehui.com.carteam.http.CM;
-import www.lvchehui.com.carteam.http.ComCb;
+import www.lvchehui.com.carteam.view.dlg.CWayDlg;
 
 @ContentView(R.layout.activity_home)
 public class HomeAct extends BaseAct{
     @ViewInject(R.id.fullscreen_content)
     private TextView m_fullscreen_content;
+    @ViewInject(R.id.iv)
+    private ImageView m_iv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Event( R.id.btn_ok)
     private void onHomeActOnClick(View v){
-        CM.getInstance().login("", "", new ComCb<LoginBean>() {
-            @Override
-            public void onSuccess(LoginBean result) {
-                m_fullscreen_content.setText("result:" + result.toString());
-            }
-            @Override
-            public void onFinished() {
-                super.onFinished();
-            }
-        });
+        new CWayDlg(this).show();
+//        CM.getInstance().login("", "", new ComCb<LoginBean>() {
+//            @Override
+//            public void onSuccess(LoginBean result) {
+//                m_fullscreen_content.setText("result:" + result.toString());
+//            }
+//            @Override
+//            public void onFinished() {
+//                super.onFinished();
+//            }
+//        });
     }
+
 }
