d94db6e7b7e3f12329a6ec7d63ec65aa57273e66
xiangbc <XiangBC123456>
2016-08-02 08:49:31 +0800

    提交实体类的更改

--- app/src/main/java/www/lvchehui/com/carteam/bean/LoginBean.java
+++ app/src/main/java/www/lvchehui/com/carteam/bean/LoginBean.java
@@ -9,11 +9,20 @@ import www.lvchehui.com.carteam.http.JRParser;
 
 /**
  * 作者：V先生 on 2016/8/1 17:30
- * 作用：
+ * 作用：基类
  */
 @HttpResponse(parser = JRParser.class)
 public class LoginBean implements Serializable{
     public int errCode;
     public String resMsg;
     public LoginEntity resData;
+
+    @Override
+    public String toString() {
+        return "LoginBean_parser{" +
+                "errCode=" + errCode +
+                ", resMsg='" + resMsg + '\'' +
+                ", resData=" + resData +
+                '}';
+    }
 }
