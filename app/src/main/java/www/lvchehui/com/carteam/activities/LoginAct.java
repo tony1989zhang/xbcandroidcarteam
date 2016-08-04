17bfc377503e95610bb395b41ceb25018f27127b
xiangbc <shenduyuxin@qq.com>
2016-08-04 16:02:26 +0800

    提交登录的页面

--- app/src/main/java/www/lvchehui/com/carteam/activities/LoginAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/activities/LoginAct.java
@@ -3,11 +3,9 @@ package www.lvchehui.com.carteam.activities;
 import android.os.Bundle;
 
 import org.xutils.view.annotation.ContentView;
-import org.xutils.view.annotation.ViewInject;
 
 import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.base.BaseAct;
-import www.lvchehui.com.carteam.view.TitleView;
 
 /**
  * 作者：V先生 on 2016/8/4 09:43
@@ -15,12 +13,9 @@ import www.lvchehui.com.carteam.view.TitleView;
  */
 @ContentView(R.layout.act_login)
 public class LoginAct extends BaseAct {
-    @ViewInject(R.id.title_view)
-    private TitleView m_title_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitleV(m_title_view,"登录");
     }
 
 }
