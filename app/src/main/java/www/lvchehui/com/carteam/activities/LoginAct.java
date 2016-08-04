129af5bbaa71aeb3fbc5015886c441fffda569ef
xiangbc <shenduyuxin@qq.com>
2016-08-04 13:26:12 +0800

    提交登陆的设置

--- app/src/main/java/www/lvchehui/com/carteam/activities/LoginAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/activities/LoginAct.java
@@ -1,10 +1,21 @@
 package www.lvchehui.com.carteam.activities;
 
+import android.os.Bundle;
+
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.base.BaseAct;
 
 /**
  * 作者：V先生 on 2016/8/4 09:43
  * 作用：登录
  */
+@ContentView(R.layout.act_login)
 public class LoginAct extends BaseAct {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
