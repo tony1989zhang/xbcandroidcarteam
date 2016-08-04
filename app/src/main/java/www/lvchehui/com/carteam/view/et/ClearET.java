c3cb58d684299395ca69ba319621ea7d7913490b
xiangbc <XiangBC123456>
2016-08-04 10:23:54 +0800

    带清除功能的文本输入框

---
+++ app/src/main/java/www/lvchehui/com/carteam/view/et/ClearET.java
@@ -0,0 +1,23 @@
+package www.lvchehui.com.carteam.view.et;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * 作者：V先生 on 2016/8/4 10:22
+ * 作用：带清除功能的文本输入框
+ */
+public class ClearET extends EditText {
+    public ClearET(Context context) {
+        super(context);
+    }
+
+    public ClearET(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClearET(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
