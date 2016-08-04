f2c11197a98cd6600687b1aa217b2da91b9944f1
xiangbc <XiangBC123456>
2016-08-04 09:45:31 +0800

    登录页面Activity

---
+++ app/src/main/java/www/lvchehui/com/carteam/activities/LoginAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.activities;
+
+import www.lvchehui.com.carteam.base.BaseAct;
+
+/**
+ * 作者：V先生 on 2016/8/4 09:43
+ * 作用：登录
+ */
+public class LoginAct extends BaseAct {
+}
