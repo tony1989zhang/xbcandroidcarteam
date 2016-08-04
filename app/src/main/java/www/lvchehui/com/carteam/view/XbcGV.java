5012b49ad23f5ae6446836f228d166041c4ef8c3
xiangbc <XiangBC123456>
2016-08-04 09:50:49 +0800

    提交本地目录

--- app/src/main/java/www/lvchehui/com/carteam/view/XbcGV.java
+++ app/src/main/java/www/lvchehui/com/carteam/view/XbcGV.java
@@ -9,7 +9,7 @@ import android.widget.GridView;
  * 作者：V先生 on 2016/8/2 18:54
  * 作用：自定义GridView 布局首页
  */
-public class XbcGv extends GridView{
+public class XbcGv extends GridView {
     public XbcGv(Context context) {
         super(context);
     }
@@ -24,10 +24,17 @@ public class XbcGv extends GridView{
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (ev.getAction() == MotionEvent.ACTION_MOVE)
-        {
+        if (ev.getAction() == MotionEvent.ACTION_MOVE) {
             return true;//禁止GridView进行滑动
         }
         return super.dispatchTouchEvent(ev);
     }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
 }
