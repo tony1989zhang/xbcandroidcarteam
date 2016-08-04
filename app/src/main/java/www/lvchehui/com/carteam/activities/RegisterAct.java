9e6799b18b39cccaa42d2003776f3bc5275f764b
xiangbc <shenduyuxin@qq.com>
2016-08-04 13:40:28 +0800

    提交注册页面的Act

---
+++ app/src/main/java/www/lvchehui/com/carteam/activities/RegisterAct.java
@@ -0,0 +1,16 @@
+package www.lvchehui.com.carteam.activities;
+
+import android.os.Bundle;
+
+import www.lvchehui.com.carteam.base.BaseAct;
+
+/**
+ * Created by 张灿能 on 2016/8/4.
+ * 作用：注册时Act
+ */
+public class RegisterAct extends BaseAct {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
