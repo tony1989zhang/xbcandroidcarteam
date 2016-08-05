7f5df5b7165113a8c4734dee7e270c5e56daec58
xiangbc <shenduyuxin@qq.com>
2016-08-05 10:37:47 +0800

    提交更改

---
+++ app/src/main/java/www/lvchehui/com/carteam/activities/ResponInfoAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.activities;
+
+import www.lvchehui.com.carteam.base.BaseAct;
+
+/**
+ * Created by 张灿能 on 2016/8/5.
+ * 作用：车队负责人信息
+ */
+public class ResponInfoAct extends BaseAct {
+}
