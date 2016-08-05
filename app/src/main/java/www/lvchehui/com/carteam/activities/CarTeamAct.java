8025daa4f1bf4dd537b509fad20484085049fb13
xiangbc <shenduyuxin@qq.com>
2016-08-05 11:07:05 +0800

    提交更改

--- app/src/main/java/www/lvchehui/com/carteam/activities/CarTeamAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/activities/CarTeamAct.java
@@ -1,11 +1,15 @@
 package www.lvchehui.com.carteam.activities;
 
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.base.BaseAct;
 
 /**
  * Created by 张灿能 on 2016/8/5.
  * 作用：车队信息
  */
+@ContentView(R.layout.act_crecar_team)
 public class CarTeamAct extends BaseAct {
 
 }
