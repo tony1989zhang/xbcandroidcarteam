7476ae504e8d39fb2dd15a234abd679d22b9d2be
xiangbc <shenduyuxin@qq.com>
2016-08-05 17:09:50 +0800

    提交正则放着

--- app/src/main/java/www/lvchehui/com/carteam/tools/RegexUtils.java
+++ app/src/main/java/www/lvchehui/com/carteam/tools/RegexUtils.java
@@ -5,7 +5,7 @@ package www.lvchehui.com.carteam.tools;
  * 作用：检测合法性
  */
 public class RegexUtils {
-    String TELEPHONE = "/^0{0,1}(13[0-9]|15[7-9]|153|156|18[7-9])[0-9]{8}$/";
+    public static final String TELEPHONE = "/^0{0,1}(13[0-9]|15[7-9]|153|156|18[7-9])[0-9]{8}$/";
     public static final String NOT_EMPTY = "(?m)^\\s*\\S+[\\s\\S]*$";
 
     private RegexUtils() {
