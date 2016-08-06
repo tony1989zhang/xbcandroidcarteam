53b00fe193edc969a9ec52b7c5b6a69b2d53c270
xiangbc <shenduyuxin@qq.com>
2016-08-06 11:40:15 +0800

    提交自定义的正则法则

--- app/src/main/java/www/lvchehui/com/carteam/tools/RegexUtils.java
+++ app/src/main/java/www/lvchehui/com/carteam/tools/RegexUtils.java
@@ -5,7 +5,7 @@ package www.lvchehui.com.carteam.tools;
  * 作用：检测合法性
  */
 public class RegexUtils {
-    public static final String TELEPHONE = "/^(((13[0-9]{1})|(18[0-9]{1})|(14[0-9]{1})|(15[0-9]{1}))+\\d{8})$/";
+    public static final String TELEPHONE = "^(((13[0-9]{1})|(18[0-9]{1})|(14[0-9]{1})|(15[0-9]{1}))+\\d{8})$";
     public static final String NOT_EMPTY = "(?m)^\\s*\\S+[\\s\\S]*$";
 
     private RegexUtils() {
