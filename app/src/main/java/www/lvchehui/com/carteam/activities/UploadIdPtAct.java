ef86d16a0bfe0575500671b5eb01f9b99bf0ca48
xiangbc <shenduyuxin@qq.com>
2016-08-06 14:27:06 +0800

    添加上传照片的类

---
+++ app/src/main/java/www/lvchehui/com/carteam/activities/UploadIdPtAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.activities;
+
+import www.lvchehui.com.carteam.base.BaseFormAct;
+
+/**
+ * Created by 张灿能 on 2016/8/6.
+ * 作用：
+ */
+public class UploadIdPtAct extends BaseFormAct {
+}
