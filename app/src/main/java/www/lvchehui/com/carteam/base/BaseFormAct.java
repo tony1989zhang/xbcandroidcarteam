8e98750fa51a94240f470b5e1a43f61c8c8ffb82
xiangbc <shenduyuxin@qq.com>
2016-08-06 09:54:04 +0800

    提交清除表单的操作

---
+++ app/src/main/java/www/lvchehui/com/carteam/base/BaseFormAct.java
@@ -0,0 +1,37 @@
+package www.lvchehui.com.carteam.base;
+
+import android.graphics.Color;
+import android.view.WindowManager;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+
+/**
+ * Created by 张灿能 on 2016/8/6.
+ * 作用:表单Act
+ */
+public class BaseFormAct extends BaseAct {
+
+    @Override
+    protected void initView() {
+        super.initView();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+    private AwesomeValidation mAwesomeValidation;
+//    RegexUtils.TELEPHONE
+    protected void setmAwesomeValidation(int v,String regex,int errMsgId){
+        mAwesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
+        mAwesomeValidation.setColor(Color.YELLOW);
+        mAwesomeValidation.addValidation(this,v,regex, errMsgId);
+        mAwesomeValidation.validate();
+//        mAwesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
+//        mAwesomeValidation.setColor(Color.YELLOW);
+//        mAwesomeValidation.addValidation(this, R.id.et_respon_sex, RegexUtils.NOT_EMPTY, R.string.err_tel);
+//        mAwesomeValidation.validate();
+    }
+    protected void clearAwe(){
+        if (null == mAwesomeValidation)
+            mAwesomeValidation.clear();
+    }
+}
