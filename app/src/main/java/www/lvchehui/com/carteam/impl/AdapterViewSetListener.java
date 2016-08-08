d67033402c30eb610fa2986288722f33937c71f9
xiangbc <shenduyuxin@qq.com>
2016-08-08 13:38:35 +0800

    传递Item值

--- app/src/main/java/www/lvchehui/com/carteam/impl/AdapterViewSetListener.java
+++ app/src/main/java/www/lvchehui/com/carteam/impl/AdapterViewSetListener.java
@@ -2,10 +2,12 @@ package www.lvchehui.com.carteam.impl;
 
 import android.view.View;
 
+import java.util.ArrayList;
+
 /**
  * Created by 张灿能 on 2016/8/8.
  * 作用：处理Custom的view设置
  */
 public interface AdapterViewSetListener {
-    void getItemView(View view);
+    <T>void getItemView(View view,ArrayList<T> list);
 }
--- app/src/main/java/www/lvchehui/com/carteam/impl/ListDlgItemClickListener.java
+++ app/src/main/java/www/lvchehui/com/carteam/impl/ListDlgItemClickListener.java
@@ -4,6 +4,6 @@ package www.lvchehui.com.carteam.impl;
  * Created by 张灿能 on 2016/8/8.
  * 作用：ListDlg点击事件
  */
-interface ListDlgItemClickListener<T> {
+public interface ListDlgItemClickListener<T> {
     void getItem(T t);
 }
