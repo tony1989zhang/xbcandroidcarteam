5a7715363e40e14a6cc699412baa6249c78339ab
xiangbc <shenduyuxin@qq.com>
2016-08-08 10:50:53 +0800

    提交对布局的更改工作。

---
+++ app/src/main/java/www/lvchehui/com/carteam/evebus/CarTeamEvent.java
@@ -0,0 +1,17 @@
+package www.lvchehui.com.carteam.evebus;
+
+/**
+ * Created by 张灿能 on 2016/8/8.
+ * 作用：接收从CarTeam的数据
+ */
+public class CarTeamEvent {
+     private boolean carTeamOnClick;
+
+    public boolean isCarTeamOnClick() {
+        return carTeamOnClick;
+    }
+
+    public void setCarTeamOnClick(boolean carTeamOnClick) {
+        this.carTeamOnClick = carTeamOnClick;
+    }
+}
