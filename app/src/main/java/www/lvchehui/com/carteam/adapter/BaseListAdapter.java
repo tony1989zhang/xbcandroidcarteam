62e788f381d180f26de6b39c82b250ea3dfc31b1
xiangbc <shenduyuxin@qq.com>
2016-08-08 16:52:41 +0800

    添加关键地址的查找

---
+++ app/src/main/java/www/lvchehui/com/carteam/adapter/BaseListAdapter.java
@@ -0,0 +1,21 @@
+package www.lvchehui.com.carteam.adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 张灿能 on 2016/6/30.
+ * 作用：ListView 基类
+ */
+public abstract class BaseListAdapter extends BaseAdapter {
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/www/lvchehui/com/carteam/evebus/PkwSearchEvent.java
@@ -0,0 +1,17 @@
+package www.lvchehui.com.carteam.evebus;
+
+/**
+ * Created by 张灿能 on 2016/8/8.
+ * 作用：
+ */
+public class PkwSearchEvent {
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
