44bd9acde9cfdee6161e008160058b558ffa360c
xiangbc <shenduyuxin@qq.com>
2016-08-09 09:44:42 +0800

    对公账户转账

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/AccountAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.module.crecarteam.activities;
+
+import www.lvchehui.com.carteam.base.BaseFormAct;
+
+/**
+ * Created by 张灿能 on 2016/8/9.
+ * 作用：对公转账
+ */
+public class AccountAct extends BaseFormAct {
+}
