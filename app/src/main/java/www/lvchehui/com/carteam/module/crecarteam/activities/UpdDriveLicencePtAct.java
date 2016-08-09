b85db871e5393384a1d62d5214f24c942ce32771
xiangbc <shenduyuxin@qq.com>
2016-08-09 18:05:34 +0800

    提交扩展

--- app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/UpdDriveLicencePtAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/UpdDriveLicencePtAct.java
@@ -12,6 +12,7 @@ import org.xutils.view.annotation.ViewInject;
 
 import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.activities.UploadIdPtAct;
+import www.lvchehui.com.carteam.http.CUtil;
 
 /**
  * Created by 张灿能 on 2016/8/9.
@@ -24,11 +25,14 @@ public class UpdDriveLicencePtAct extends UploadIdPtAct {
     @ViewInject(R.id.btn_submit_pt2)
     private Button m_btn_submit_pt2;
     private static final int PT_NUM_SEC = 2;
+    private int pTtype = 0;//两张照片的时候可以用到
 
     @Override
     protected void initView() {
         super.initView();
-        initTextView("上传道路经营许可证", "道路经营许可证号", "上传道路经营许可证图片", "请在道路经营许可证复印件上，按如下要求完成并拍照上传。注：手写“仅用于享包车认证”，并在复印件上加盖红色公章。", "http://img5.imgtn.bdimg.com/it/u=4022192071,1728188534&fm=21&gp=0.jpg");
+        initTextView("上传行驶证", "行驶证号", "上传行驶证图片", "请在行驶证复印件上，按如下要求完成并拍照上传。\n" +
+                "注：手写“仅用于享包车认证”，并在复印件上加盖红色公章。。", "http://img5.imgtn.bdimg.com/it/u=4022192071,1728188534&fm=21&gp=0.jpg");
+        CUtil.loadImage(m_iv_photo2, "http://img3.imgtn.bdimg.com/it/u=367402233,876693516&fm=21&gp=0.jpg");
     }
 
     @Event(R.id.btn_submit_pt2)
@@ -38,6 +42,13 @@ public class UpdDriveLicencePtAct extends UploadIdPtAct {
         mInstance.showDialog(this);
         pTtype = PT_NUM_SEC;
 
+    }
+
+    @Override
+    public void btn_submitPt() {
+        pTtype = 0;
+        super.btn_submitPt();
+        showToast("父类的调用" + pTtype);
     }
 
     @Override
