bf6f48907e69ad6cbb47877e018b100f799a1533
xiangbc <shenduyuxin@qq.com>
2016-08-10 11:31:50 +0800

    提交车内外部车身照片布局

--- app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/UpdCarInPtAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/UpdCarInPtAct.java
@@ -1,10 +1,12 @@
 package www.lvchehui.com.carteam.module.crecarteam.activities;
 
 import www.lvchehui.com.carteam.activities.UploadIdPtAct;
+import www.lvchehui.com.carteam.base.BaseFormAct;
 
 /**
  * Created by 张灿能 on 2016/8/10.
  * 作用：车内部情况
  */
-public class UpdCarInPtAct extends UploadIdPtAct {
+public class UpdCarInPtAct extends BaseFormAct {
+
 }
