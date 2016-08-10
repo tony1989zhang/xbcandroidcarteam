1f68e252db7596a992c481d329cb4f1d466ac2bb
xiangbc <shenduyuxin@qq.com>
2016-08-10 10:12:22 +0800

    提交享包车的设置

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/UpdRoadTranPtAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.module.crecarteam.activities;
+
+import www.lvchehui.com.carteam.activities.UploadIdPtAct;
+
+/**
+ * Created by 张灿能 on 2016/8/10.
+ * 作用：上传道路运输证
+ */
+public class UpdRoadTranPtAct extends UploadIdPtAct {
+}
