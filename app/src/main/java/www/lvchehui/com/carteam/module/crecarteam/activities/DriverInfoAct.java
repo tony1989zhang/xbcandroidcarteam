31ba84d0d63a5723975cd1147036aeceddb0ec47
xiangbc <shenduyuxin@qq.com>
2016-08-10 15:47:44 +0800

    提交司机信息的更改

--- app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/DriverInfoAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/module/crecarteam/activities/DriverInfoAct.java
@@ -1,10 +1,14 @@
 package www.lvchehui.com.carteam.module.crecarteam.activities;
 
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.base.BaseAct;
 
 /**
  * Created by 张灿能 on 2016/8/5.
  * 作用：司机信息
  */
+@ContentView(R.layout.act_driver_info)
 public class DriverInfoAct extends BaseAct {
 }
