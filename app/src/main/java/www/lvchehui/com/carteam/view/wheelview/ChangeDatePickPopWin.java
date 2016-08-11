54ead5972ffc4727bc56b9801809fbe31c2a3ab1
xiangbc <shenduyuxin@qq.com>
2016-08-11 10:07:23 +0800

    提交自定义高仿ios效果

---
+++ app/src/main/java/www/lvchehui/com/carteam/view/wheelview/ChangeDatePickPopWin.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.view.wheelview;
+
+import android.widget.PopupWindow;
+
+/**
+ * Created by 张灿能 on 2016/8/11.
+ * 作用：自定义防IOS弹框效果
+ */
+public class ChangeDatePickPopWin extends PopupWindow {
+}
