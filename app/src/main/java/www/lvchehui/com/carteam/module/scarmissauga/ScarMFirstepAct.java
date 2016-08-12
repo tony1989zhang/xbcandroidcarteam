eb5647ece346d9d56b4e477d97e0b14d98f78f08
xiangbc <shenduyuxin@qq.com>
2016-08-12 17:25:46 +0800

    提交发布顺风车的初步需求

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/scarmissauga/ScarMFirstepAct.java
@@ -0,0 +1,14 @@
+package www.lvchehui.com.carteam.module.scarmissauga;
+
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
+import www.lvchehui.com.carteam.base.BaseFormAct;
+
+/**
+ * Created by 张灿能 on 2016/8/12.
+ * 作用：发布顺风车
+ */
+@ContentView(R.layout.act_scarm_firstep)
+public class ScarMFirstepAct extends BaseFormAct {
+}
---
+++ app/src/main/java/www/lvchehui/com/carteam/module/scarmissauga/ScarMnextAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.module.scarmissauga;
+
+import www.lvchehui.com.carteam.base.BaseFormAct;
+
+/**
+ * Created by 张灿能 on 2016/8/12.
+ * 作用：
+ */
+public class ScarMnextAct extends BaseFormAct {
+}
