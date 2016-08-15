9770bc0a8156e107e9db109cf096ecdaba5b272f
xiangbc <shenduyuxin@qq.com>
2016-08-15 16:45:40 +0800

    输入确定

--- app/src/main/java/www/lvchehui/com/carteam/module/scarmissauga/ScarMnextAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/module/scarmissauga/ScarMnextAct.java
@@ -1,10 +1,14 @@
 package www.lvchehui.com.carteam.module.scarmissauga;
 
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
 import www.lvchehui.com.carteam.base.BaseFormAct;
 
 /**
  * Created by 张灿能 on 2016/8/12.
  * 作用：
  */
+@ContentView(R.layout.act_scarmnext)
 public class ScarMnextAct extends BaseFormAct {
 }
