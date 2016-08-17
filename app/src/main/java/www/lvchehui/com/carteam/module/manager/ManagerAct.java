e8be3115881d0b7a99b91d6799ec2362d84461c5
xiangbc <shenduyuxin@qq.com>
2016-08-17 13:24:09 +0800

    提交管理页面

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/manager/ManagerAct.java
@@ -0,0 +1,45 @@
+package www.lvchehui.com.carteam.module.manager;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+
+import www.lvchehui.com.carteam.R;
+import www.lvchehui.com.carteam.base.BaseAct;
+import www.lvchehui.com.carteam.view.TitleView;
+
+/**
+ * Created by 张灿能 on 2016/8/12.
+ * 作用：权限管理
+ */
+@ContentView(R.layout.act_manager)
+public class ManagerAct extends BaseAct {
+    @ViewInject(R.id.title_view)
+    private TitleView m_title_view;
+
+    @Override
+    protected void initView() {
+        super.initView();
+        setTitleV(m_title_view,"权限管理");
+    }
+
+    @Event({R.id.rl_quote,R.id.rl_scar_missauga,R.id.rl_replace_driver,R.id.rl_replace_vehicle})
+    private void managerOnClick(View v){
+        switch (v.getId())
+        {
+            case R.id.rl_quote:
+                break;
+            case R.id.rl_scar_missauga:
+                break;
+            case R.id.rl_replace_driver:
+                break;
+            case R.id.rl_replace_vehicle:
+                break;
+        }
+    }
+}
