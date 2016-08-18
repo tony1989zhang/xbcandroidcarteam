eb506b4298f1ec011d01b51d1d65665aa2b55ae6
xiangbc <shenduyuxin@qq.com>
2016-08-18 09:09:40 +0800

    提交更改

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/setting/SettingAct.java
@@ -0,0 +1,14 @@
+package www.lvchehui.com.carteam.module.setting;
+
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
+import www.lvchehui.com.carteam.base.BaseAct;
+
+/**
+ * Created by 张灿能 on 2016/8/18.
+ * 作用：
+ */
+@ContentView(R.layout.act_setting)
+public class SettingAct extends BaseAct {
+}
