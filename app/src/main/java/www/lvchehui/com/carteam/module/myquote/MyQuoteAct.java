eca77be058c300996e2e57989c73bc5278e90bc6
xiangbc <shenduyuxin@qq.com>
2016-08-19 10:00:45 +0800

    提交报价布局

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/myquote/MyQuoteAct.java
@@ -0,0 +1,20 @@
+package www.lvchehui.com.carteam.module.myquote;
+
+import android.os.Bundle;
+
+import org.xutils.view.annotation.ContentView;
+
+import www.lvchehui.com.carteam.R;
+import www.lvchehui.com.carteam.base.BaseAct;
+
+/**
+ * Created by 张灿能 on 2016/8/19.
+ * 作用：我的报价
+ */
+@ContentView(R.layout.act_my_quote)
+public class MyQuoteAct extends BaseAct {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
