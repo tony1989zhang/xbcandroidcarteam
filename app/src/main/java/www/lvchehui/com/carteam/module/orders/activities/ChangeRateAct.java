9728dd408dc76167aea5eaf5f7248b181e09f65e
xiangbc <shenduyuxin@qq.com>
2016-08-23 09:45:49 +0800

    提交费用变更

---
+++ app/src/main/java/www/lvchehui/com/carteam/module/orders/activities/ChangeRateAct.java
@@ -0,0 +1,10 @@
+package www.lvchehui.com.carteam.module.orders.activities;
+
+import www.lvchehui.com.carteam.base.BaseFormAct;
+
+/**
+ * Created by 张灿能 on 2016/8/23.
+ * 作用：费用变更
+ */
+public class ChangeRateAct extends BaseFormAct {
+}
