92e4ebebd62b51964691f79ac2830602c8120a5f
xiangbc <shenduyuxin@qq.com>
2016-08-24 08:49:15 +0800

    提交信用等级

--- app/src/main/java/www/lvchehui/com/carteam/module/XbcCreditListAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/module/XbcCreditListAct.java
@@ -4,24 +4,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import org.xutils.common.Callback;
 import org.xutils.view.annotation.ContentView;
-import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import www.lvchehui.com.carteam.R;
-import www.lvchehui.com.carteam.base.BaseAct;
 import www.lvchehui.com.carteam.base.BaseListAct;
 import www.lvchehui.com.carteam.base.BasePageAdapter;
 import www.lvchehui.com.carteam.bean.LoginBean;
 import www.lvchehui.com.carteam.bean.TextBean;
 import www.lvchehui.com.carteam.http.CM;
-import www.lvchehui.com.carteam.tools.XgoLog;
 
 /**
  * Created by 张灿能 on 2016/8/23.
@@ -29,6 +24,12 @@ import www.lvchehui.com.carteam.tools.XgoLog;
  */
 @ContentView(R.layout.act_xbc_credit)
 public class XbcCreditListAct extends BaseListAct<LoginBean> {
+    @Override
+    protected void initViews() {
+        super.initViews();
+        setTitleV(mTitleView,"享等级");
+    }
+
     @Override
     protected List convertToBeanList(LoginBean loginBean) {
         ArrayList<TextBean> arr =  new ArrayList<>();
