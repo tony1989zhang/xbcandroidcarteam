60ae62f7e9cfa9e1cfc88571c51c8985c649b4cd
xiangbc <shenduyuxin@qq.com>
2016-08-25 09:50:43 +0800

    提交更改

--- app/src/main/java/www/lvchehui/com/carteam/activities/SplanshAct.java
+++ app/src/main/java/www/lvchehui/com/carteam/activities/SplanshAct.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import org.xutils.image.ImageOptions;
@@ -87,9 +89,20 @@ public class SplanshAct extends BaseAct {
                         //设置支持gif
                 .setIgnoreGif(false)
                 .build();
-        x.image().loadDrawable("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1472088194&di=db4dca288e8967cf60d7a5971703f4b1&src=http://img.zcool.cn/community/01313f56c3d5546ac7256cb094ff05.jpg", options, new ComCb<Drawable>() {
+        //https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1472088194&di=db4dca288e8967cf60d7a5971703f4b1&src=http://img.zcool.cn/community/01313f56c3d5546ac7256cb094ff05.jpg
+        //http://7xlh8k.com1.z0.glb.clouddn.com/%E8%BD%A6%E9%98%9F%EF%BC%8D%E5%B9%BF%E5%91%8A.png
+        x.image().loadDrawable("http://7xlh8k.com1.z0.glb.clouddn.com/%E8%BD%A6%E9%98%9F%EF%BC%8D%E5%B9%BF%E5%91%8A.png", options, new ComCb<Drawable>() {
             @Override
             public void onSuccess(Drawable result) {
+
+                DisplayMetrics dm = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(dm);
+                ViewGroup.LayoutParams layoutParams = m_imageView2.getLayoutParams();
+                int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+                m_imageView2.setLayoutParams(layoutParams);
+                m_imageView2.setMaxWidth(dm.widthPixels);
+                m_imageView2.setMaxHeight((int)(dm.widthPixels*5));
                 m_imageView2.setImageDrawable(result);
                 m_imageView2.setOnClickListener(new View.OnClickListener() {
                     @Override
