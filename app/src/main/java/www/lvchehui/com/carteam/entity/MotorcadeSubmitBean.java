9faf263fe94629fdcd5327cae026ae79e5a4820c
xiangbc <shenduyuxin@qq.com>
2016-08-26 17:06:00 +0800

    提交更改

--- app/src/main/java/www/lvchehui/com/carteam/entity/MotorcadeSubmitBean.java
+++ app/src/main/java/www/lvchehui/com/carteam/entity/MotorcadeSubmitBean.java
@@ -1,8 +1,10 @@
 package www.lvchehui.com.carteam.entity;
 
+import www.lvchehui.com.carteam.bean.BaseBean;
+
 /**
  * Created by 张灿能 on 2016/8/26.
  * 作用：
  */
-public class MotorcadeSubmitBean {
+public class MotorcadeSubmitBean extends BaseBean {
 }
